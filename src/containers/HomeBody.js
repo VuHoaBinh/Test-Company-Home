cc0e501ffa9b99bed0be4b1b595ecb25e28d94db
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-11-11 17:33:59 +0700

    menu-littlebody-finished

--- src/containers/HomeBody.js
+++ src/containers/HomeBody.js
@@ -4,17 +4,17 @@ import ArticlesTips from "./Sections/ArticlesTips";
 import BestValueTrips from "./Sections/BestValueTrips";
 import PopularDestination from "./Sections/PopularDestinations";
 import WhyChooseUs from "./Sections/WhyChooseUs";
-
+import "./HomeBody.scss";
 class HomeBody extends Component {
   render() {
     return (
-      <>
+      <div className="wrapper">
         <Search />
         <PopularDestination />
         <BestValueTrips />
         <WhyChooseUs />
         <ArticlesTips />
-      </>
+      </div>
     );
   }
 }
--- src/containers/Sections/PopularDestinations.js
+++ src/containers/Sections/PopularDestinations.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
-
+import "./PopularDestination.scss";
 class PopularDestination extends Component {
   render() {
-    return <p>PopularDestination</p>;
+    return (
+      <div className="wrapper">
+        <div className="title">
+          <h2>Popular Destinations</h2>
+          <h5>World's best tourist destinations</h5>
+        </div>
+        <div className="content">
+          <div class="row">
+            <div class="col-3">
+              <img
+                src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Tokyo_Dollarphotoclub_72848283-copy-700x466.jpg"
+                alt="Myphoto"
+              ></img>
+              <p>Tokyo</p>
+            </div>
+
+            <div class="col-3">
+              <img
+                src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/bf1202aedf2c5b6a57d379575619a488-700x466.jpg"
+                alt="Myphoto"
+              ></img>
+              <p>Seoul</p>
+            </div>
+            <div class="col-3">
+              <img
+                src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/1600x1200-4-700x466.jpg"
+                alt="Myphoto"
+              ></img>
+              <p>Paris</p>
+            </div>
+            <div class="col-3">
+              <img
+                src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/pexels-photo-1-700x466.jpg"
+                alt="Myphoto"
+              ></img>
+              <p>London</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
